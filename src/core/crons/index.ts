189bd43a019c3342e8580334d4c551445f89fdaa
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2025-11-03 03:28:52 +0000

    fix: dynamic importing

--- src/core/crons/index.ts
+++ src/core/crons/index.ts
@@ -5,7 +5,13 @@ import path from 'path';
   try {
     const eventsHandlers = fs
       .readdirSync(path.resolve(__dirname))
-      .filter(command => command.includes('.cron'));
+      .filter(command => command.includes('.cron'))
+      .filter(
+        command =>
+          (command.endsWith('.ts') || command.endsWith('.js')) &&
+          !command.endsWith('.d.ts'),
+      )
+      .filter(command => !command.endsWith('.d.ts'));
 
     await Promise.allSettled(
       eventsHandlers.map(async handler => {
--- src/core/events/index.ts
+++ src/core/events/index.ts
@@ -7,8 +7,13 @@ import { client } from '../discord/client.discord';
   try {
     const eventsHandlers = fs
       .readdirSync(path.resolve(__dirname))
-      .filter(command => command.includes('.event'));
-
+      .filter(command => command.includes('.event'))
+      .filter(
+        command =>
+          (command.endsWith('.ts') || command.endsWith('.js')) &&
+          !command.endsWith('.d.ts'),
+      )
+      .filter(command => !command.endsWith('.d.ts'));
     await Promise.allSettled(
       eventsHandlers.map(async handler => {
         console.debug(`Loading event ${handler}`);
